918e809086d59205811c2b5d310e6fbd2ca93819
Manjusri S <smanjusri3010@gmail.com>
2025-06-15 14:59:37 +0530

    Digital-Nurture-4.0-Deep Skilling

---
+++ Week 1_Design_Patterns_and_Principles/06_Proxy Pattern/ProxyTest.java
@@ -0,0 +1,7 @@
+public class ProxyTest {
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test.jpg");
+        image.display();
+        image.display();
+    }
+}
