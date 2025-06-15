eba97a5e029c1d985b45468255dd61a89affaf55
Manjusri S <smanjusri3010@gmail.com>
2025-06-15 15:16:04 +0530

    Digital-Nurture-4.0-Deep Skilling

---
+++ Week 1_Design_Patterns_and_Principles/10_MVC Pattern/StudentView.java
@@ -0,0 +1,5 @@
+public class StudentView {
+    public void displayStudentDetails(String name, String id, String grade) {
+        System.out.println("Student: " + name + ", ID: " + id + ", Grade: " + grade);
+    }
+}
