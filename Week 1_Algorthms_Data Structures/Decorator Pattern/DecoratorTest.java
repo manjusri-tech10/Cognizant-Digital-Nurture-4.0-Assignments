2e712ec55526d6ac9a2b99630a49e2dff0d4dc4f
Manjusri S <smanjusri3010@gmail.com>
2025-06-15 11:10:56 +0530

    Digital Nurture 4.0 Deep Skilling

---
+++ Week 1_Algorthms_Data Structures/Decorator Pattern/DecoratorTest.java
@@ -0,0 +1,8 @@
+public class DecoratorTest {
+    public static void main(String[] args) {
+        Notifier notifier = new SlackNotifierDecorator(
+                                new SMSNotifierDecorator(
+                                    new EmailNotifier()));
+        notifier.send("New alert received!");
+    }
+}
