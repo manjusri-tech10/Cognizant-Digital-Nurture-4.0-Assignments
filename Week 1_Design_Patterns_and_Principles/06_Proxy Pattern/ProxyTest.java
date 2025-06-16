e5a0d3c2e2454b298f3c8483a00c09925270ca9c
MANJU SRI <smanjusri3010@gmail.com>
2025-06-16 20:18:14 +0530

    Digital-Nurture-4.0-Deep Skilling

--- Week 1_Design_Patterns_and_Principles/06_Proxy Pattern/ProxyTest.java
+++
@@ -1,7 +0,0 @@
-public class ProxyTest {
-    public static void main(String[] args) {
-        Image image = new ProxyImage("test.jpg");
-        image.display();
-        image.display();
-    }
-}
--- Week 1_Design_Patterns_and_Principles/07_Observer Pattern/WebApp.java
+++
@@ -1,5 +0,0 @@
-public class WebApp implements Observer {
-    public void update(float price) {
-        System.out.println("WebApp received stock price: " + price);
-    }
-}
