d68d9867004a09ea9b187ce53acb11f6d3d4a61a
Manjusri S <smanjusri3010@gmail.com>
2025-06-16 20:54:33 +0530

    Digital-Nurture-4.0-Deep Skilling

--- DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 1_Data_Structures_and_Algorithms/01_Inventory Management System/MainInventory.java
+++
@@ -1,8 +0,0 @@
-public class MainInventory {
-    public static void main(String[] args) {
-        Inventory inv = new Inventory();
-        inv.addProduct(new Product(1, "Laptop", 10, 60000));
-        inv.updateProduct(1, 12, 58000);
-        inv.deleteProduct(1);
-    }
-}
