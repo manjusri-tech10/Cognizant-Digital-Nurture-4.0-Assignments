9be54e31c569c44d0a2812b597f71ba2c05844f1
Manjusri S <smanjusri3010@gmail.com>
2025-06-17 21:03:02 +0530

    Added MainSearch class to test linear and binary search implementations

--- DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 1_Data_Structures_and_Algorithms/02_E-commerce Platform Search Function/MainSearch.java
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 1_Data_Structures_and_Algorithms/02_E-commerce Platform Search Function/MainSearch.java
@@ -1,16 +1,19 @@
 import java.util.Arrays;
-
 public class MainSearch {
     public static void main(String[] args) {
         Product[] products = {
-            new Product(1, "Phone", "Electronics"),
-            new Product(2, "Shirt", "Apparel"),
-            new Product(3, "Laptop", "Electronics")
+                new Product(1, "Phone", "Electronics"),
+                new Product(2, "Shirt", "Apparel"),
+                new Product(3, "Laptop", "Electronics")
         };
 
         Arrays.sort(products, (a, b) -> a.productName.compareTo(b.productName));
         SearchEngine s = new SearchEngine();
-        s.linearSearch(products, "Laptop");
-        s.binarySearch(products, "Shirt");
+
+        int linearResult = s.linearSearch(products, "Laptop");
+        int binaryResult = s.binarySearch(products, "Shirt");
+
+        System.out.println("Linear Search: Found at index " + linearResult);
+        System.out.println("Binary Search: Found at index " + binaryResult);
     }
 }
