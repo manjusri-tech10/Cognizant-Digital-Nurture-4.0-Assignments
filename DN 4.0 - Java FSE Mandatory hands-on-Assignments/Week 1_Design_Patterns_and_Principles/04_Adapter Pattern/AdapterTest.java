2a7b019f5e24b54fba085e85c133d9c44a13499b
Manjusri S <smanjusri3010@gmail.com>
2025-06-17 19:17:10 +0530

    Delete DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 1_Design_Patterns_and_Principles/04_Adapter Pattern directory

--- DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 1_Design_Patterns_and_Principles/04_Adapter Pattern/AdapterTest.java
+++
@@ -1,6 +0,0 @@
-public class AdapterTest {
-    public static void main(String[] args) {
-        PaymentProcessor processor = new StripeAdapter(new StripeGateway());
-        processor.processPayment(250.0);
-    }
-}
