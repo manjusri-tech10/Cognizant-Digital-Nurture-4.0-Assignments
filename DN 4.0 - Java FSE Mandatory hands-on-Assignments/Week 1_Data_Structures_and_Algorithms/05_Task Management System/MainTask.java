664bbdb4c300f32eb3bc58f4627fd9dab3f5a056
Manjusri S <smanjusri3010@gmail.com>
2025-06-17 20:17:44 +0530

    Digital-Nurture-4.0-Deep Skilling

--- DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 1_Data_Structures_and_Algorithms/05_Task Management System/MainTask.java
+++
@@ -1,9 +0,0 @@
-public class MainTask {
-    public static void main(String[] args) {
-        TaskManager m = new TaskManager();
-        m.addTask(new Task(1, "Write Report", "Pending"));
-        m.searchTask(1);
-        m.traverseTasks();
-        m.deleteTask(1);
-    }
-}
