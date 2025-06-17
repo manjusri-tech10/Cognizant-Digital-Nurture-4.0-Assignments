29a612a06ac512c03d7786fa6b147d9ebf3de1dd
Manjusri S <smanjusri3010@gmail.com>
2025-06-17 21:29:15 +0530

    Update ExcelDocumentFactory.java

--- DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 1_Design_Patterns_and_Principles/02_Factory Method Pattern/ExcelDocumentFactory.java
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 1_Design_Patterns_and_Principles/02_Factory Method Pattern/ExcelDocumentFactory.java
@@ -1,5 +1,5 @@
 public class ExcelDocumentFactory extends DocumentFactory {
     public Document createDocument() {
-        return new ExcelDocument();
+        return new ExcelDocument();//
     }
 }
