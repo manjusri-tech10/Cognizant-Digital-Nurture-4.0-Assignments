18d9b27c8741fe049a926639fd5fe0e9dcec36db
Manjusri S <smanjusri3010@gmail.com>
2025-06-17 21:33:44 +0530

    Update WordDocumentFactory.java

--- DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 1_Design_Patterns_and_Principles/02_Factory Method Pattern/WordDocumentFactory.java
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 1_Design_Patterns_and_Principles/02_Factory Method Pattern/WordDocumentFactory.java
@@ -1,5 +1,5 @@
 public class WordDocumentFactory extends DocumentFactory {
     public Document createDocument() {
-        return new WordDocument();
+        return new WordDocument();//
     }
 }
