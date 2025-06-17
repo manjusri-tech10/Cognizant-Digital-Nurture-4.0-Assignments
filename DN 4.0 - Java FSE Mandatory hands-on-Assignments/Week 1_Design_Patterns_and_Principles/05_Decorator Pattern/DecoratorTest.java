7b1c77de4707c15bbc824003daba71231c4261f1
Manjusri S <smanjusri3010@gmail.com>
2025-06-17 19:17:42 +0530

    Delete DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 1_Design_Patterns_and_Principles/05_Decorator Pattern directory

--- DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 1_Design_Patterns_and_Principles/05_Decorator Pattern/DecoratorTest.java
+++
@@ -1,8 +0,0 @@
-public class DecoratorTest {
-    public static void main(String[] args) {
-        Notifier notifier = new SlackNotifierDecorator(
-                                new SMSNotifierDecorator(
-                                    new EmailNotifier()));
-        notifier.send("New alert received!");
-    }
-}
