c531578ff4d5cf9dee1c0c28e4883c09e56ed831
Manjusri S <smanjusri3010@gmail.com>
2025-06-30 11:31:18 +0530

    Added setter method in BookService for dependency injection

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 3_Spring Core and Maven/02_Implementing Dependency Injection/BookService.java
@@ -0,0 +1,17 @@
+package com.library.service;
+import com.library.repository.BookRepository;
+import java.util.List;
+public class BookService {
+    private BookRepository bookRepository;
+    // Setter method for dependency injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+  public void showBooks() {
+        System.out.println("BookService: Retrieving list of books...");
+        List<String> books = bookRepository.getBooks();
+        for (String book : books) {
+            System.out.println(book);
+        }
+    }
+}
