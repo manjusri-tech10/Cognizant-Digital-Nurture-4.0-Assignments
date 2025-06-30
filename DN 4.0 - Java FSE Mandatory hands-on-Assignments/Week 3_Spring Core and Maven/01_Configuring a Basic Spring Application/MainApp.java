3eaca8f079a5b3f89549ff6eaff49c194071d91f
Manjusri S <smanjusri3010@gmail.com>
2025-06-30 11:11:50 +0530

    Implemeted MainApp to print execution steps and invoke showBooks method

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 3_Spring Core and Maven/01_Configuring a Basic Spring Application/MainApp.java
@@ -0,0 +1,21 @@
+package com.library;
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Starting LibraryManagement Application...");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        System.out.println("Spring context loaded successfully.");
+
+        BookService bookService = context.getBean("bookService", BookService.class);
+        System.out.println("BookService bean retrieved.");
+
+        System.out.println("Calling showBooks() method...");
+        bookService.showBooks();
+
+        System.out.println("Application execution completed.");
+    }
+}
