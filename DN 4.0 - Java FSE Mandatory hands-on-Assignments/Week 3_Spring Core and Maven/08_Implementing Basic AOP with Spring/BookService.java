a3c4a8320827afbc741e4f46c7fda1ef22bc4df0
Manjusri S <smanjusri3010@gmail.com>
2025-07-01 18:48:51 +0530

    Verified AOP by applying LoggingAspect to BookService methods

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 3_Spring Core and Maven/08_Implementing Basic AOP with Spring/BookService.java
@@ -0,0 +1,23 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void showBooks() {
+        System.out.println("BookService: Displaying books...");
+        for (String book : bookRepository.getBooks()) {
+            System.out.println("📘 " + book);
+        }
+    }
+}
