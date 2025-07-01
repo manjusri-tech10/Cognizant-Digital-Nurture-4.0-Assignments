8349e8774664c819ef8973d1a271895b02224ce0
Manjusri S <smanjusri3010@gmail.com>
2025-07-01 10:46:36 +0530

    Annotated BookService with @Service and added @Autowired setter for BookRepository

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 3_Spring Core and Maven/06_Configuring Beans with Annotations/BookService.java
@@ -0,0 +1,18 @@
+package com.library.service;
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class BookService {
+    private BookRepository bookRepository;
+    @Autowired  // Setter injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    public void showBooks() {
+        System.out.println("BookService: Retrieving list of books...");
+        for (String book : bookRepository.getBooks()) {
+            System.out.println("📗 " + book);
+        }
+    }
+}
