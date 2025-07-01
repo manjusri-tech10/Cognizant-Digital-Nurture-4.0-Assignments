b5cd59bdfce4cb6f46411fbe6bb2d5c3e0e0200f
Manjusri S <smanjusri3010@gmail.com>
2025-07-01 11:15:31 +0530

    Updated BookRepository to return a sample list of books for constructor and setter injection testing

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 3_Spring Core and Maven/07_Implementing Constructor and Setter Injection/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library.repository;
+import java.util.Arrays;
+import java.util.List;
+public class BookRepository {
+    public List<String> getBooks() {
+        return Arrays.asList("Spring in Action", "Java Concurrency", "Design Patterns");
+    }
+}
