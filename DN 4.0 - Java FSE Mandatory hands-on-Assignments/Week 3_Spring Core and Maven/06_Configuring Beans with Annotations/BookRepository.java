f0e8345e1be1476888d5596cd2befc5b15e35259
Manjusri S <smanjusri3010@gmail.com>
2025-07-01 10:48:40 +0530

    Annotated BookRepository with Repository to enable automatic bean detection

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 3_Spring Core and Maven/06_Configuring Beans with Annotations/BookRepository.java
@@ -0,0 +1,10 @@
+package com.library.repository;
+import org.springframework.stereotype.Repository;
+import java.util.Arrays;
+import java.util.List;
+@Repository
+public class BookRepository {
+    public List<String> getBooks() {
+        return Arrays.asList("The Alchemist", "Clean Code", "Effective Java", "Refactoring");
+    }
+}
