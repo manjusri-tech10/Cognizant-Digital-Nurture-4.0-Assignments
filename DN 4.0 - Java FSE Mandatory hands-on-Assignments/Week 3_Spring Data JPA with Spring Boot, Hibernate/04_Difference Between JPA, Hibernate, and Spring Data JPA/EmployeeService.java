190ae54e2472053f7622a49bf3c559a466cae218
Manjusri S <smanjusri3010@gmail.com>
2025-07-04 20:46:47 +0530

    Implemented EmployeeService with addEmployee() using Spring Data JPA

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 3_Spring Data JPA with Spring Boot, Hibernate/04_Difference Between JPA, Hibernate, and Spring Data JPA/EmployeeService.java
@@ -0,0 +1,17 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import com.example.model.Employee;
+import com.example.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Transactional
+    public void addEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+}
