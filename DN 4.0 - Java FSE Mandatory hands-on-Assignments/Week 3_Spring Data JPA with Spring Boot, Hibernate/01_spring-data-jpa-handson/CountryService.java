c9390c507b4cd9e34653f99fba6ef49eaa228dde
Manjusri S <smanjusri3010@gmail.com>
2025-07-04 20:23:27 +0530

    Implemented CountryService with getAllCountries() method

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 3_Spring Data JPA with Spring Boot, Hibernate/01_spring-data-jpa-handson/CountryService.java
@@ -0,0 +1,10 @@
+@Service
+public class CountryService {
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
