45df7d3a5fde32907913e856af0912a808861a6c
Manjusri S <smanjusri3010@gmail.com>
2025-07-05 12:59:39 +0530

    Created Country entity with coCode and coName fields

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 3_Spring Data JPA with Spring Boot, Hibernate/spring-data-jpa-handson/HandsOn 1/05_Implement services for managing Country /Country.java
@@ -0,0 +1,29 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "country")
+public class Country {
+  
+  @Id
+  private String coCode;
+
+  private String coName;
+
+  // no-arg constructor
+  public Country() {}
+
+  public Country(String coCode, String coName) {
+    this.coCode = coCode;
+    this.coName = coName;
+  }
+
+  public String getCoCode() { return coCode; }
+  public void setCoCode(String coCode) { this.coCode = coCode; }
+
+  public String getCoName() { return coName; }
+  public void setCoName(String coName) { this.coName = coName; }
+}
