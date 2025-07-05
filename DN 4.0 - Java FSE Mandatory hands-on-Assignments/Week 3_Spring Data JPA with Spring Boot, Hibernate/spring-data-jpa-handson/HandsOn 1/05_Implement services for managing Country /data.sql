57e0e92016d0cd5a80535ce5927ab0f27e846242
Manjusri S <smanjusri3010@gmail.com>
2025-07-05 12:58:41 +0530

    Added data to populate country table with global country list

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 3_Spring Data JPA with Spring Boot, Hibernate/spring-data-jpa-handson/HandsOn 1/05_Implement services for managing Country /data.sql
@@ -0,0 +1,7 @@
+DELETE FROM country;
+INSERT INTO country (co_code, co_name) VALUES
+  ('AF','Afghanistan'),
+  ('AL','Albania'),
+  ('DZ','Algeria'),
+  ('ZW','Zimbabwe'),
+  ('AX','Åland Islands');
