bf900e468b7a801130a12ec84f177a836cc3f6b4
Manjusri S <smanjusri3010@gmail.com>
2025-07-06 15:40:46 +0530

    Add EmployeeService with get() and save() methods

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 3_Spring Data JPA with Spring Boot, Hibernate/spring-data-jpa-handson/HandsOn 2/02_Demonstrate implementation of OR Mapping/EmployeeService.java
@@ -0,0 +1,16 @@
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Transactional
+    public Employee get(int id) {
+        return employeeRepository.findById(id).get();
+    }
+
+    @Transactional
+    public void save(Employee employee) {
+        employeeRepository.save(employee);
+    }
+}
