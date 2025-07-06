986dbfe9986a07d9eaa99c902bd009a1dcb223dc
Manjusri S <smanjusri3010@gmail.com>
2025-07-06 14:53:42 +0530

    Implemented findCountryByCode method in CountryService with @Transactional and exception handling

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 3_Spring Data JPA with Spring Boot, Hibernate/spring-data-jpa-handson/HandsOn 1/06_ country based on country code /CountryService.java
@@ -0,0 +1,26 @@
+package com.cognizant.springlearn.service;
+
+import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.service.exception.CountryNotFoundException;
+import com.cognizant.springlearn.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public Country findCountryByCode(String countryCode) throws CountryNotFoundException {
+        Optional<Country> result = countryRepository.findById(countryCode);
+        if (!result.isPresent()) {
+            throw new CountryNotFoundException("Country not found with code: " + countryCode);
+        }
+        return result.get();
+    }
+}
