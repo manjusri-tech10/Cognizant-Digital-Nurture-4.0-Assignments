bbdf690b2bd1e5393ff772995d6413e879fc1395
Manjusri S <smanjusri3010@gmail.com>
2025-07-06 15:04:54 +0530

    Add addCountry method in CountryService using save()

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 3_Spring Data JPA with Spring Boot, Hibernate/spring-data-jpa-handson/HandsOn 1/07_Add a new country/CountryService.java
@@ -0,0 +1,31 @@
+package com.cognizant.springlearn.service;
+
+import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.repository.CountryRepository;
+import com.cognizant.springlearn.service.exception.CountryNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public Country findCountryByCode(String countryCode) throws CountryNotFoundException {
+        Optional<Country> result = countryRepository.findById(countryCode);
+        if (!result.isPresent()) {
+            throw new CountryNotFoundException("Country not found with code: " + countryCode);
+        }
+        return result.get();
+    }
+
+    @Transactional
+    public void addCountry(Country country) {
+        countryRepository.save(country);
+    }
+}
