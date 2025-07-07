96fbbc005d2c40c26c85721cf919d2cb96c24f3a
Manjusri S <smanjusri3010@gmail.com>
2025-07-07 20:34:09 +0530

    Implement EmployeeDao to read employee list from XML

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 4_Spring REST using Spring Boot 3/3. spring-rest-handson/static employee list data using spring xml configuration/EmployeeDao.java
@@ -0,0 +1,20 @@
+package com.cognizant.dao;
+
+import com.cognizant.model.Employee;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.util.ArrayList;
+
+public class EmployeeDao {
+
+    private static ArrayList<Employee> EMPLOYEE_LIST;
+
+    public EmployeeDao() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("employee.xml");
+        EMPLOYEE_LIST = (ArrayList<Employee>) context.getBean("employeeList");
+    }
+
+    public ArrayList<Employee> getAllEmployees() {
+        return EMPLOYEE_LIST;
+    }
+}
