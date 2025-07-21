ab8f1898a7b8ce1c796c7d9b74032dbeac06204b
Manjusri S <smanjusri3010@gmail.com>
2025-07-21 19:52:26 +0530

    Create Home.js

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 6_React/2. ReactJS-HOL/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to the Home Page of Student Management Portal</h2>
+    </div>
+  );
+}
+
+export default Home;
