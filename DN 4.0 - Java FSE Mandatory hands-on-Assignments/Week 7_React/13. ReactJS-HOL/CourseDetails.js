7df6845fcafcf0c6aabc69aa4455ae241707e623
Manjusri S <smanjusri3010@gmail.com>
2025-07-30 20:34:20 +0530

    Add CourseDetails component using map() for course list rendering

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 7_React/13. ReactJS-HOL/CourseDetails.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function CourseDetails() {
+  const courses = [
+    { name: "Angular", date: "4/5/2021" },
+    { name: "React", date: "6/3/20201" },
+  ];
+  return (
+    <div>
+      <h2>Course Details</h2>
+      {courses.map((course, index) => (
+        <div key={index}>
+          <h3>{course.name}</h3>
+          <p>{course.date}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default CourseDetails;
