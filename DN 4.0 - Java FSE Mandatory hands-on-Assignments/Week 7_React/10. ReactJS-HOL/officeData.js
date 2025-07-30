90b8499f620e53d2ec759d0d99232b10c73c347b
Manjusri S <smanjusri3010@gmail.com>
2025-07-30 19:59:30 +0530

    Added office list data file

---
+++ DN 4.0 - Java FSE Mandatory hands-on-Assignments/Week 7_React/10. ReactJS-HOL/officeData.js
@@ -0,0 +1,16 @@
+const officeList = [
+  {
+    name: "DBS",
+    rent: 50000,
+    address: "Chennai",
+    image: "https://via.placeholder.com/250x200?text=DBS+Office"
+  },
+  {
+    name: "Regus",
+    rent: 70000,
+    address: "Bangalore",
+    image: "https://via.placeholder.com/250x200?text=Regus+Office"
+  }
+];
+
+export default officeList;
